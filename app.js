048cd93bc188be34106ebfc606d401d50a401b6c
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2019-04-15 17:30:30 +0530

    Bootstrap Nav Bar and Cards added to page

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressHbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
