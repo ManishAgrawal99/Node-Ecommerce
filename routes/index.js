f2f9936c542cd48a67b8bd957b886a861a42b2fe
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2019-04-15 22:58:49 +0530

    Retrieving data from DB and dynamic display of products using handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var Product = require('../models/product');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+router.get('/', function (req, res, next) {
+    //Searching the database for all the products
+    var products = Product.find({}).exec((err, products)=>{
+        if(err){
+            res.send(err);
+        }
+        else{
+            console.log(products);
+            res.render('shop/index', { title: 'Shopping Cart' , products:products }); 
+        }
+    })
 });
 
 module.exports = router;
