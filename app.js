51237ef849beeac412f9f6b24d11c6a82428cd26
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2019-05-13 01:39:21 +0530

    Signup Page created and added to routes and CSRF protection added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose'); 
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 
@@ -22,6 +23,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'manishSecretKey', resave: false, saveUninitialized: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var Product = require('../models/product');
+var csrf = require('csurf');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -16,5 +20,14 @@ router.get('/', function (req, res, next) {
         }
     })
 });
+
+router.get('/user/signup', function(req, res, next){
+    res.render('user/signup', { csrfToken: req.csrfToken() });
+    console.log(req.csrfToken());
+})
+
+router.post('/user/signup', function(req, res, next){
+    res.redirect('/')
+})
 
 module.exports = router;
